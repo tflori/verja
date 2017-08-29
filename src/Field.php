7976e9387c4a0ebb0cc307c28ab516de7c7c6433
Thomas Flori <t.flori@sportradar.com>
2017-08-29 10:59:41 +0200

    add filtering for field

--- src/Field.php
+++ src/Field.php
@@ -4,5 +4,76 @@ namespace Verja;
 
 class Field
 {
+    /** @var FilterInterface[] */
+    protected $filters = [];
 
+    /** @var ValidatorInterface[] */
+    protected $validators = [];
+
+    /**
+     * Append $filter to the list of filters
+     *
+     * @param FilterInterface $filter
+     * @return $this
+     */
+    public function appendFilter(FilterInterface $filter)
+    {
+        array_push($this->filters, $filter);
+        return $this;
+    }
+
+    /**
+     * Prepend $filter to the list of filters
+     *
+     * @param FilterInterface $filter
+     * @return $this
+     */
+    public function prependFilter(FilterInterface $filter)
+    {
+        array_unshift($this->filters, $filter);
+        return $this;
+    }
+
+    /**
+     * Alias for appendFilter
+     *
+     * @param FilterInterface $filter
+     * @return Field
+     * @see Field::appendFilter()
+     */
+    public function addFilter(FilterInterface $filter)
+    {
+        return $this->appendFilter($filter);
+    }
+
+//    public function appendValidator(ValidatorInterface $validator)
+//    {
+//        array_push($this->validators, $validator);
+//        return $this;
+//    }
+//
+//    public function prependValidator(ValidatorInterface $validator)
+//    {
+//        array_unshift($this->validators, $validator);
+//        return $this;
+//    }
+//
+//    public function addValidator(ValidatorInterface $validator)
+//    {
+//        return $this->appendValidator($validator);
+//    }
+
+    /**
+     * Filter $value with predefined filters
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function filter($value)
+    {
+        foreach ($this->filters as $filter) {
+            $value = $filter->filter($value);
+        }
+        return $value;
+    }
 }
---
+++ tests/Field/FilterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Verja\Test\Field;
+
+use PHPUnit\Framework\TestCase;
+use Verja\Field;
+use Verja\Filter\Trim;
+
+class FilterTest extends TestCase
+{
+    /** @test */
+    public function executesAllFilters()
+    {
+        $field = new Field();
+        $field->addFilter(new Trim(' '));
+        $field->addFilter(new Trim('/'));
+
+        $result = $field->filter(' body/');
+
+        self::assertSame('body', $result);
+    }
+
+    /** @test */
+    public function executesInSpecificOrder()
+    {
+        $field = new Field();
+        $field->appendFilter(new Trim(' '));
+        $field->prependFilter(new Trim('/'));
+
+        $result = $field->filter('/ body');
+
+        self::assertSame('body', $result);
+    }
+}
