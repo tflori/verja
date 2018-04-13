ca3068cc7cfadd449f9d73c6fab5c0207572e297
Thomas Flori <thflori@gmail.com>
2018-04-13 07:52:17 +0200

    make validator and filter invokable

--- src/FilterInterface.php
+++ src/FilterInterface.php
@@ -14,6 +14,15 @@ interface FilterInterface
     public function filter($value, array $context = []);
 
     /**
+     * Call the filter is an alias for filter
+     *
+     * @param mixed $value
+     * @param array $context
+     * @return mixed
+     */
+    public function __invoke($value, array $context = []);
+
+    /**
      * Assign filter to $field
      *
      * @param Field $field
